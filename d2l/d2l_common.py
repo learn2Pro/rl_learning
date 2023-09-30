9dba31b8d26da124410a0c0496fe83583225c901
cloudtang <cloudtang@tencent.com>
2023-09-30 13:30:10 +0800

    [feat]: attention mechanism fst class

--- d2l/d2l_common.py
+++ d2l/d2l_common.py
@@ -707,5 +707,22 @@ def bleu(pred_seq, label_seq, k):
     return score
 
 
+def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5), cmap='Reds'):
+    """Show heatmaps of matrices."""
+    num_rows, num_cols, _, _ = matrices.shape
+    fig, axes = plt.subplots(
+        num_rows, num_cols, figsize=figsize, sharex=True, sharey=True, squeeze=False)
+    for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
+        for j, (ax, matrix) in enumerate(zip(row_axes, row_matrices)):
+            pcm = ax.imshow(matrix.detach().numpy(), cmap=cmap)
+            if i == num_rows - 1:
+                ax.set_xlabel(xlabel)
+            if j == 0:
+                ax.set_ylabel(ylabel)
+            if titles:
+                ax.set_title(titles)
+    fig.colorbar(pcm, ax=axes, shrink=0.6)
+
+
 if __name__ == "__main__":
     print(1)
