71e1adc7ad863d64ac2e4d050faa1a9d5eda1125
learn2pro <druidlangde@gmail.com>
2025-04-30 14:44:21 +0800

    [feat]: gqa implementation [feat]: rlhf impl

--- llm/selfgpt/buddygpt.py
+++ llm/selfgpt/buddygpt.py
@@ -9,34 +9,61 @@ from transformers import PreTrainedModel
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 FLASH = 0
 
-# rope
-def precompute_freqs_cis(dim, end, theta=10000.0):
-    freqs = theta ** -(torch.arange(0, dim, 2)[:dim//2].float() / dim)
-    t = torch.arange(end)
-    freqs = torch.outer(t, freqs) # m * \theta
-    # freqs = t * freqs
-    freqs = torch.polar(torch.ones_like(freqs), freqs) # cos(m * \theta) + jsin(m * \theta)
-    return freqs
+# # rope
+# def precompute_freqs_cis(dim, end, theta=10000.0):
+#     freqs = theta ** -(torch.arange(0, dim, 2)[:dim//2].float() / dim)
+#     t = torch.arange(end)
+#     freqs = torch.outer(t, freqs) # m * \theta
+#     # freqs = t * freqs
+#     freqs = torch.polar(torch.ones_like(freqs), freqs) # cos(m * \theta) + jsin(m * \theta)
+#     return freqs
 
-# 2. 为广播 reshape freqs
-def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
-    if freqs_cis.shape[0] > x.shape[1]:
-        freqs_cis = freqs_cis[:x.shape[1]]
-    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
-    shape = [1 if i != 1 and i != x.ndim - 1 else x.shape[i] for i in range(x.ndim)]
-    return freqs_cis.view(*shape).to(x.device)
+# # 2. 为广播 reshape freqs
+# def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+#     if freqs_cis.shape[0] > x.shape[1]:
+#         freqs_cis = freqs_cis[:x.shape[1]]
+#     assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+#     shape = [1 if i != 1 and i != x.ndim - 1 else x.shape[i] for i in range(x.ndim)]
+#     return freqs_cis.view(*shape).to(x.device)
 
-def apply_rotary_emb(q, k, freqs):
-    xq = torch.view_as_complex(q.view(*q.shape[:-1], -1, 2)) # batch, seq_len, n_head, dim//2
-    xk = torch.view_as_complex(k.view(*k.shape[:-1], -1, 2)) # batch, seq_len, n_head, dim//2
+# def apply_rotary_emb(q, k, freqs):
+#     xq = torch.view_as_complex(q.view(*q.shape[:-1], -1, 2)) # batch, seq_len, n_head, dim//2
+#     xk = torch.view_as_complex(k.view(*k.shape[:-1], -1, 2)) # batch, seq_len, n_head, dim//2
     
-    freqs_cis = reshape_for_broadcast(freqs, xq) # freqs_cis.shape = (1,x.shape[1],1,x.shape[-1])
+#     freqs_cis = reshape_for_broadcast(freqs, xq) # freqs_cis.shape = (1,seq_len,1,dim)
 
-    xq_out = torch.view_as_real(xq * freqs_cis).flatten(3) # batch, seq_len, n_head, dim
-    xk_out = torch.view_as_real(xk * freqs_cis).flatten(3) # batch, seq_len, n_head, dim
+#     xq_out = torch.view_as_real(xq * freqs_cis).flatten(3) # batch, seq_len, n_head, dim
+#     xk_out = torch.view_as_real(xk * freqs_cis).flatten(3) # batch, seq_len, n_head, dim
 
-    return xq_out.type_as(q), xk_out.type_as(k)
+#     return xq_out.type_as(q), xk_out.type_as(k)
     
+class RotaryEmbedding(nn.Module):
+    def __precompute_freqs_cis(self, dim, max_seq_len, theta):
+        assert dim%2 == 0
+        freqs = theta ** -(torch.arange(0, dim ,2).float() / dim)
+        t = torch.arange(max_seq_len)
+        freqs = torch.outer(t, freqs) # (seq_len, dim/2)
+        freqs = torch.polar(torch.ones_like(freqs), freqs) # cos(m*\theta) + jsin(m*\theat)
+        return freqs
+        
+    def __init__(self, dim, max_seq_len=2048, theta=10000.0):
+        super().__init__()
+        self.dim = dim
+        self.freqs = self.__precompute_freqs_cis(dim, max_seq_len, theta)
+
+    def apply_rotary_emb(self, q, k=None):
+        seq_len, dim = q.size(1), q.size(-1) # batch, n_head, seq_len, n_embed
+        freqs_cis = self.freqs[None, :seq_len, None, :dim//2].contiguous().to(q.device)
+        xq = torch.view_as_complex(q.view(*q.shape[:-1], -1, 2))
+        xq_out = torch.view_as_real(xq * freqs_cis).flatten(3)
+        if k is not None:
+            xk = torch.view_as_complex(k.view(*k.shape[:-1], -1, 2))
+            xk_out = torch.view_as_real(xk * freqs_cis).flatten(3)
+            return xq_out, xk_out
+        else:
+            return xq_out           
+
+        
 @dataclass
 class GPTConfig(PretrainedConfig):
     n_block: int
@@ -44,10 +71,12 @@ class GPTConfig(PretrainedConfig):
     n_head: int
     n_embed: int
     n_vocab: int = 50257
+    n_kv_head: int = 2
     model_type: str = 'buddygpt'
     pad_token_id=None,
     bos_token_id=None,
     eos_token_id=50256,
+    rope = None,
     keys_to_ignore_at_inference = ["past_key_values"]
 
     def __init__(self, **kwargs):
@@ -63,6 +92,49 @@ class SwiGLU(nn.Module):
         x1, x2 = x.chunk(2, dim=-1)  # split last dim into two
         return F.silu(x1) * x2  # silu == swish
         
+        
+class GQA(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.n_head = config.n_head
+        self.n_kv_head = config.n_kv_head
+        self.n_embed = config.n_embed
+        self.head_dim = self.n_embed // self.n_head
+        self.kv_head_dim = self.head_dim * self.n_kv_head
+        self.repeat_factor = self.n_head // self.n_kv_head
+        self.q_proj = nn.Linear(self.n_embed, self.n_embed)
+        self.k_proj = nn.Linear(self.n_embed, self.kv_head_dim)
+        self.v_proj = nn.Linear(self.n_embed, self.kv_head_dim)
+        self.out_proj = nn.Linear(self.n_embed, self.n_embed)
+        self.rope = config.rope
+        self.register_buffer('tril', torch.tril(torch.ones(config.n_block, config.n_block)).view(1,1,config.n_block, config.n_block))
+
+    def forward(self, x):
+        B, T, _ = x.shape
+        q = self.q_proj(x).view(B, T, self.n_head, -1) # B, T, n_head, n_embed
+        k = self.k_proj(x).view(B, T, self.n_kv_head, -1) # B, T, n_kv_head, n_embed
+        v = self.v_proj(x).view(B, T, self.n_kv_head, -1) # B, T, n_kv_head, n_embed
+
+        xq, xk = self.rope.apply_rotary_emb(q), self.rope.apply_rotary_emb(k)
+
+        xq = xq.transpose(1, 2) # B, n_head, T, n_embed
+        xk = xk.transpose(1, 2) # B, n_kv_head, T, n_embed
+        xv = v.transpose(1, 2) # B, n_kv_head, T, n_embed
+
+        xk = xk.repeat_interleave(self.repeat_factor, dim=1) # B, n_head, T, n_embed
+        xv = xv.repeat_interleave(self.repeat_factor, dim=1) # B, n_head, T, n_embed
+
+        if FLASH:
+            o_attn = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        else:
+            qk = torch.matmul(xq, xk.transpose(-2, -1))
+            qk = qk.masked_fill(self.tril[:,:,:T,:T]==0, float('-inf'))
+            qk = F.softmax(qk, dim=-1) * (self.n_embed ** -0.5)
+            o_attn = qk @ xv # B, n_head, T, n_embed
+        o_attn = o_attn.transpose(1, 2).contiguous().view(B, T, -1)
+        return self.out_proj(o_attn)
+
+        
 class SelfCausalAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -71,7 +143,7 @@ class SelfCausalAttention(nn.Module):
         self.n_block = config.n_block
         self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
         self.c_proj = nn.Linear(config.n_embed, config.n_embed)
-        self.freqs_cis = precompute_freqs_cis(config.n_embed // config.n_head, config.n_block * 2)
+        self.rope = RotaryEmbedding(dim=config.n_embed)
         self.register_buffer('tril', torch.tril(torch.ones(config.n_block, config.n_block)).view(1,1,config.n_block,config.n_block))
 
     def forward(self, x):
@@ -81,14 +153,14 @@ class SelfCausalAttention(nn.Module):
         q = q.view(B, T, self.n_head, -1).transpose(1, 2) # B, n_head, n_block, n_embed//n_head
         k = k.view(B, T, self.n_head, -1).transpose(1, 2) # B, n_head, n_block, n_embed//n_head
         v = v.view(B, T, self.n_head, -1).transpose(1, 2) # B, n_head, n_block, n_embed//n_head
-        q, k = apply_rotary_emb(q, k, self.freqs_cis)
+        q, k = self.rope.apply_rotary_emb(q, k, self.freqs_cis)
         if FLASH:
             o_attn = F.scaled_dot_product_attention(q, k, v, is_causal=True)
         else:
             qk = q @ k.transpose(-2, -1)
             qk = qk.masked_fill(self.tril[:,:,:T,:T] == 0, float('-inf'))
             o_attn = (F.softmax(qk, dim=-1) * (self.n_embed ** -0.5)) @ v
-        o_attn = o_attn.transpose(1, 2).contiguous().view(B, T, -1)
+        o_attn = o_attn.transpose(1, 2).view(B, T, -1).contiguous()
         y = self.c_proj(o_attn)
         return y
 
@@ -96,27 +168,27 @@ class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.n_embed = config.n_embed
-        self.ln1 = nn.Linear(config.n_embed, 4 * config.n_embed)
-        self.gelu = nn.GELU()
-        self.ln2 = nn.Linear(4 * config.n_embed, config.n_embed)
+        self.ln1 = nn.Linear(config.n_embed, 2 * config.n_embed)
+        self.silu = SwiGLU()
+        self.ln2 = nn.Linear(config.n_embed, config.n_embed)
 
     def forward(self, x):
         x = self.ln1(x)
-        x = self.gelu(x)
+        x = self.silu(x)
         x = self.ln2(x)
         return x
 
 class Layer(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.mha = SelfCausalAttention(config)
+        self.mha = GQA(config)
         self.mlp = MLP(config)
-        self.norm1 = nn.RMSNorm(config.n_embed)
-        self.norm2 = nn.RMSNorm(config.n_embed)
+        self.pre_norm = nn.RMSNorm(config.n_embed)
+        self.post_norm = nn.RMSNorm(config.n_embed)
 
     def forward(self, x):
-        x = x + self.mha(self.norm1(x))
-        x = x + self.mlp(self.norm2(x))
+        x = x + self.mha(self.pre_norm(x))
+        x = x + self.mlp(self.post_norm(x))
         return x
 
 class BuddyGPT(PreTrainedModel):
@@ -127,12 +199,14 @@ class BuddyGPT(PreTrainedModel):
         super().__init__(config)
         self.config = config
         self.n_vocab = config.n_vocab
+        config.rope = RotaryEmbedding(config.n_embed)
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.n_vocab, config.n_embed),
-            wpe = nn.Embedding(config.n_block, config.n_embed),
             layers = nn.ModuleList([Layer(config) for _ in range(config.n_layer)]),
             ln_norm = nn.RMSNorm(config.n_embed),
+            rope = config.rope,
         ))
+
         self.lm_head = nn.Linear(config.n_embed, config.n_vocab, bias=False)
         self.eos_token_id = config.eos_token_id
         self.transformer.wte.weight = self.lm_head.weight
@@ -156,8 +230,8 @@ class BuddyGPT(PreTrainedModel):
         B, T = input_ids.size()
         pos = torch.arange(0, T, dtype=torch.long, device=device)
         token_embed = self.transformer.wte(input_ids)
-        pos_embed = self.transformer.wpe(pos)
-        x = token_embed + pos_embed
+        # pos_embed = self.transformer.wpe(pos)
+        x = token_embed
         for layer in self.transformer.layers:
             x = layer(x)
         x = self.transformer.ln_norm(x)
